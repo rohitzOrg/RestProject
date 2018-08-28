536487b9092a189c593fb9fc870fdf9258e26b32
rohitzGit <38519671+rohitzGit@users.noreply.github.com>
2018-08-28 10:08:11 +0530

    update

--- src/main/java/com/org/navz/responseModel/MainResponseModel.java
+++ src/main/java/com/org/navz/responseModel/MainResponseModel.java
@@ -1,5 +1,13 @@
 package com.org.navz.responseModel;
 
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.OneToMany;
+
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
@@ -8,6 +16,12 @@ public class MainResponseModel {
 
 	  @JsonProperty
 	  private RestResponse RestResponse;
+	  
+	  @Id
+	    @GeneratedValue
+	    @Column(name = "CATEGORY_ID")
+	  private Long id;
+	  
 	  
 	  public RestResponse getRestResponse() {
 	    return RestResponse;
@@ -20,10 +34,18 @@ public class MainResponseModel {
 	  public MainResponseModel(){
 	    
 	  }
+	  
+	  @OneToMany(cascade = CascadeType.ALL)
+	    @JoinTable(
+	            name = "CATEGORY_ARTICLE",
+	            joinColumns = @JoinColumn(name = "CATEGORY_ID"),
+	            inverseJoinColumns = @JoinColumn(name = "ARTICLE_ID")
+	   )
 
 	  @Override
 	  public String toString() {
 	    return "Response [RestResponse=" + RestResponse + "]";
 	  }
 
+	 
 }
